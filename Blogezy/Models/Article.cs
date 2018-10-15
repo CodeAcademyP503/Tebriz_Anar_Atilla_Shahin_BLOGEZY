98ad39e71dea862d1a67304bafdf888592313740
sahincabbar <36867588+sahincabbar@users.noreply.github.com>
2018-10-15 20:45:44 +0400

    Models added

---
+++ Blogezy/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blogezy.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        public string HeaderInfo { get; set; }
+
+
+    }
+}
